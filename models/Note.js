cbeeab87a6f041f3458bdfb8fe591541f3345e3a
Vaishnavi Naik <23be0234@sipnaengg.ac.in>
2025-11-01 22:11:01 +0530

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -1,9 +1,13 @@
+const mongoose = require("mongoose");
+
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   subject: { type: String, required: true },
   fileUrl: { type: String, required: true },
   uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: false },
-  uploadedByName: { type: String }, // ✅ add this line
+  uploadedByName: { type: String }, // ✅ store username directly
   role: { type: String },
   uploadDate: { type: Date, default: Date.now },
 });
+
+module.exports = mongoose.model("Note", noteSchema);
