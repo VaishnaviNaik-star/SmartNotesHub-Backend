4619b57b5c4e502b8a524c972a53dced3e0b4c3a
Vaishnavi Naik <23be0234@sipnaengg.ac.in>
2025-11-01 22:50:53 +0530

    Update runSeeder.js

--- runSeeder.js
+++ runSeeder.js
@@ -1,17 +1,1 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const seedDemoNotes = require("./seedDemoNotes"); // path to your file
 
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(async () => {
-    console.log("✅ Connected to MongoDB");
-    await seedDemoNotes(); // call the seeder
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection failed:", err);
-    process.exit(1);
-  });
