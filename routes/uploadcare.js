7dc5c732b2ce8f688c029afc5afb98df0a82eda8
Vaishnavi Naik <23be0234@sipnaengg.ac.in>
2025-11-01 20:58:36 +0530

    Update uploadcare.js

--- routes/uploadcare.js
+++ routes/uploadcare.js
@@ -1,6 +1,6 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv";
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
 
 dotenv.config();
 const router = express.Router();
@@ -34,4 +34,4 @@ router.post("/store", async (req, res) => {
   }
 });
 
-export default router;
+module.exports = router;
